bdaa5a49dfda947ea22a51b0de11f669301c48a4
Muhammad Ali <az2310116@gmail.com>
2025-06-11 22:42:21 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,111 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from sqlalchemy import create_engine
+
+engine = create_engine("mysql+pymysql://root:root@localhost/data")
+df = pd.read_sql("SELECT * FROM all_cities", engine)
+
+#print(df.head())
+#print(df.shape)
+#print(df["city_name"].value_counts())
+#print("Name of columns:\n", df.columns)
+#print("Data Types:\n", df.dtypes)
+#print("Missing Values:\n", df.isnull().sum())
+#with pd.option_context('display.max_columns', None, 'display.width', None):
+    #print("Summary:\n\n", df.describe())
+
+'''
+avg_price = df.groupby("city_name")["price"].mean().round(2).sort_values(ascending=False)
+print("\nAverage price in $ according to", avg_price)
+
+#visualization
+plt.bar(avg_price.index, avg_price.values)
+plt.title("Price Analysis")
+plt.ylabel('Average $')
+plt.xlabel("City")
+plt.tight_layout()
+plt.show()
+'''
+
+'''
+room_counts = df.groupby(["city_name", "room_type"]).size().unstack()
+print("\nAvailability per Room Type per City:")
+print(room_counts)
+
+#visualization
+room_counts.plot(kind='bar', figsize=(10, 6))
+plt.title('Availability per Room Type per City')
+plt.xlabel('City')
+plt.ylabel('Number of Listings')
+plt.grid(axis='y')
+plt.xticks(rotation=0)
+plt.tight_layout()
+plt.show()
+'''
+
+'''
+avg_price_room = df.groupby('room_type')['price'].mean().sort_values(ascending=False)
+print("Room type average price:")
+print(avg_price_room)
+
+#visualization
+plt.bar(avg_price_room.index, avg_price_room.values)
+plt.title("Room_type price analysis")
+plt.ylabel('Average $')
+plt.xlabel("Room_type")
+plt.tight_layout()
+plt.show()
+'''
+
+'''
+avg_price_room_city = df.groupby(['city_name', 'room_type'])['price'].mean().unstack().round(2)
+print("Room type average price per city:")
+print(avg_price_room_city)
+
+#visualization
+avg_price_room_city.plot(kind='bar', figsize=(10, 6))
+plt.title('Room type average price per city')
+plt.xlabel('City')
+plt.ylabel('Average Price $')
+plt.grid(axis='y')
+plt.xticks(rotation=0)
+plt.tight_layout()
+plt.show()
+'''
+
+'''
+avg_cleanliness = df.groupby('city_name')['cleanliness_rating'].mean().sort_values(ascending=False)
+print("Average cleanliness rating out of 10 per city")
+print(avg_cleanliness)
+
+#visualization
+plt.barh(avg_cleanliness.index, avg_cleanliness.values)
+plt.title("Cleanliness analysis")
+plt.ylabel('City')
+plt.xlabel("Average Cleanliness Rating")
+plt.tight_layout()
+plt.show()
+'''
+
+'''
+avg_guest_sat = df.groupby('city_name')['guest_satisfaction_overall'].mean().sort_values(ascending=False)
+print("Average Guest Satisfaction out of 100 per city")
+print(avg_guest_sat)
+
+#visualization
+plt.barh(avg_guest_sat.index, avg_guest_sat.values)
+plt.title("Guest Satisfaction analysis")
+plt.ylabel('City')
+plt.xlabel("Average Guest Satisfaction")
+plt.tight_layout()
+plt.show()
+'''
+
+'''
+correlation = df[['price', 'guest_satisfaction_overall']].corr()
+correlation2 = df[['cleanliness_rating', 'guest_satisfaction_overall']].corr()
+correlation3 = df[['city_center_distance', 'guest_satisfaction_overall']].corr()
+correlation4 = df[['metro_distance', 'guest_satisfaction_overall']].corr()
+print(correlation, "\n", correlation2, "\n", correlation3, "\n", correlation4)
+'''
